02b68718c6d1a2d44dc2020eefecb2268a662dff
David Almanza <david.almanza@dotcreek.com>
2019-05-27 10:36:54 -0600

    Redirect to dashboard or login if user is logged or not

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import NavMain from '../NavMain';
+import { userLogged } from '../../utils';
 
 function Home() {
+
+  if (userLogged()) {
+    window.location.href = '/dashboard';
+  } else {
+    window.location.href = '/login';
+  }
 
   return(
     <div>
