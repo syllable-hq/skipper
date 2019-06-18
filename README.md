20177d7270500f8ade8f1e9e09978b20fd6d336b
Eric Chaves <erchaves@gmail.com>
2019-06-18 12:17:18 -0400

    updating env var names

--- README.md
+++ README.md
@@ -34,10 +34,11 @@ The app is running on [netlify.com](https://app.netlify.com/). The website will 
 Use proper env variables values
 
 ```
-REACT_APP_API_KEY=XXXXxxxx
-REACT_APP_AUTH_DOMAIN=xxxxXXXX.firebaseapp.com
-REACT_APP_DATABASE_URL=https://xxxXXXX.firebaseio.com
-REACT_APP_PROJECT_ID=xxxxXXXX
-REACT_APP_STORAGE_BUCKET=xxxxXXXX.appspot.com
-REACT_APP_MESSAGING_SENDER_ID=xxxxXXXX
+API_KEY=XXXXxxxx
+AUTH_DOMAIN=xxxxXXXX.firebaseapp.com
+DATABASE_URL=https://xxxXXXX.firebaseio.com
+PROJECT_ID=xxxxXXXX
+STORAGE_BUCKET=xxxxXXXX.appspot.com
+MESSAGING_SENDER_ID=xxxxXXXX
+APP_ID=xxxxxxxx
 ```
--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import { USER_ID } from '../constants';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 class Firebase {
