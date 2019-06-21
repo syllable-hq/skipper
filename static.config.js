d17e51bc3553c02506a4eb63d7da982b40565fbf
Eric Chaves <erchaves@gmail.com>
2019-06-21 17:15:15 -0400

    cleanup unused code from old project

--- static.config.js
+++ static.config.js
@@ -47,10 +47,6 @@ export default {
       component: 'src/Components/Signup'
     },
     {
-      path: '/projects',
-      component: 'src/Components/Projects',
-    },
-    {
       path: '/about',
       component: 'src/Components/About',
       noindex: true,
@@ -75,7 +71,7 @@ export default {
           <link rel="preload" as="font" href="/fonts/Matter-Regular.woff" type="font/woff" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="/fonts/Matter-SemiBold.woff2" type="font/woff2" crossOrigin="anonymous"/>
           <link rel="preload" as="font" href="/fonts/Matter-SemiBold.woff" type="font/woff" crossOrigin="anonymous"/>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous" />
 
           <title>{siteData.title}</title>
