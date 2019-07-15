16a9439e109e5e9c89ba75c674c204ee7da03768
Eric Chaves <erchaves@gmail.com>
2019-07-15 13:58:08 -0400

    Revert "testing new razzle which might not need the heroku plugin"

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const razzleHeroku = require('razzle-heroku');
+
 module.exports = {
-  plugins: ['scss']
+  plugins: ['scss'],
+  modify(config, { target, dev }, webpack) {
+    config = razzleHeroku(config, {target, dev}, webpack);
+
+    return config;
+  },
 };
